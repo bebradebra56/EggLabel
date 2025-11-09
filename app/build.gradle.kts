fb089aeaf2c53fbe296aba54d2654df22a74ce8c
fraim <fraimework@gmail.com>
2025-11-09 12:45:46 +0700

    update design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.egsi.labsi.sog"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
